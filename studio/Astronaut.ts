87973ab69a809606c2f5d89cb93c307b815293f7
Jason <jwrounds@usfca.edu>
2021-04-07 12:20:56 -0500

    outlined Astronaut, Cargo, and Rocket properties and constructors

--- studio/Astronaut.ts
+++ studio/Astronaut.ts
@@ -1,3 +1,10 @@
-export class Astronaut {
+import { Payload } from './Payload';
 
+export class Astronaut implements Payload {
+  name: string;
+  massKg: number;
+  constructor(name: string, massKg: number) {
+    this.name = name;
+    this.massKg = massKg;
+  }
 }
--- studio/Rocket.ts
+++ studio/Rocket.ts
@@ -1,3 +1,15 @@
-export class Rockets {
-  
+import { Astronaut } from './Astronaut';
+import { Cargo } from './Cargo';
+
+export class Rocket {
+  name: string;
+  totalCapacityKg: number;
+  cargoItems: Cargo[];
+  astronauts: Astronaut[];
+
+  constructor(name: string, totalCapacityKg: number) {
+    this.name = name;
+    this.totalCapacityKg = totalCapacityKg;
+  }
+
 }
